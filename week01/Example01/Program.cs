3f2c9535cb58ca429ffbaa5c7917ad04c05831e8
cuishuning <cuishuning@gmail.com>
2019-09-01 13:38:37 +0800

    Add project files.

---
+++ week01/Example01/Program.cs
@@ -0,0 +1,24 @@
+﻿///<summary>
+///说明:代码可以在GitHub下载
+///下载地址:https://github.com/AndrewChui/CSharpiCourse
+///作者:Cui Shuning
+///提示:请使用https访问Github,系统在上传时，自动删除了s！！
+///</summary> 
+
+using System;
+
+namespace Examp01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var x = Convert.ToInt32(s);
+            s = Console.ReadLine();
+            var y = Convert.ToInt32(s);
+            var z = x + y;
+            Console.WriteLine(z);
+        }
+    }
+}
