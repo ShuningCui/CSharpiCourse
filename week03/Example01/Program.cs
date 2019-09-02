07eba141be3204b15242836a9fac37f15574916e
cuishuning <cuishuning@gmail.com>
2019-09-02 12:45:13 +0800

    Add week03

---
+++ week03/Example01/Program.cs
@@ -0,0 +1,77 @@
+﻿///<summary>
+///说明:代码可以在GitHub下载
+///下载地址:https://github.com/AndrewChui/CSharpiCourse
+///作者:Cui Shuning
+///提示:请使用https访问Github,系统在上传时，自动删除了s！！
+///</summary>
+
+using System;
+
+
+namespace Example01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero heroOne = new Hero(); //通过类创建对象
+            heroOne.AddLife(); //调用对象的成员方法
+            //使用属性
+            heroOne.Life = 90;  
+            var life = heroOne.Life;
+        }
+    }
+    /// <summary>
+    /// 简单的类的示例
+    /// </summary>
+    class Hero
+    {
+        private string name;//私有成员
+        private int life = 50;  //声明时可以给出初值
+        /// <summary>
+        /// 属性，通过属性存取私有成员的值
+        /// </summary>
+        public int Life
+        {
+            get { return life; }
+            set
+            {
+                life = value < 0 ? 0 : (value > 100 ? 100 : value);
+            }
+
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        /// <summary>
+        /// 匿名属性
+        /// </summary>
+        public int Age { get; set; }
+        
+        /// <summary>
+        /// 类中一个普通的方法（函数）
+        /// </summary>
+        public void AddLife()
+        {
+            life++;
+        }
+        /// <summary>
+        /// 构造方法（构造函数）
+        /// </summary>
+        public Hero()
+        {
+            life = 0;
+            name = "";
+        }
+        /// <summary>
+        /// 一个类只能有一个析构方法（函数）。 
+        ///析构方法既没有修饰符，也没有参数
+        /// </summary>
+        ~Hero()
+        {
+
+        }
+    }
+}
---
+++ week03/Homework03/Program.cs
@@ -0,0 +1,93 @@
+﻿///<summary>
+///说明:代码可以在GitHub下载
+///下载地址:https://github.com/AndrewChui/CSharpiCourse
+///作者:Cui Shuning
+///提示:请使用https访问Github,系统在上传时，自动删除了s！！
+///</summary> 
+/*
+ * 定义并实现一个公民类Citizen，
+ * 该类包括的特征信息有：
+ * 身份证号ID、姓名Name、性别Gender、
+ * 年龄Age、籍贯Birthplace、家庭住址Familyaddress属性以
+ * 及构造方法（不含参数）、
+ * 输入公民信息方法Input以及输出公民信息方法Print，
+ * 要求能够对该类对象进行初始化、输入和输出操作。
+ */
+using System;
+
+
+namespace Homework03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Citizen c1 = new Citizen();
+            c1.Input("610101205012120000", "旺财", "火星", "交大", 2, Gender.female);
+            c1.Print();
+        }
+    }
+    /// <summary>
+    /// 性别的枚举
+    /// </summary>
+    enum Gender
+    {
+        male,
+        female,
+        x
+    }
+
+    class Citizen
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            //年龄的值如果不正确则设置为-1
+            set { age = value > 0 && value < 130 ? value : -1; }
+        }
+
+        public Gender Gender { get; set; }
+        public string BirthPlace { get; set; }
+        public string FamilyAddress { get; set; }
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        public Citizen()
+        {
+            ID = Name = BirthPlace = FamilyAddress = "";
+            Gender = Gender.x;
+            Age = 20;
+        }
+        /// <summary>
+        /// 输入方法
+        /// </summary>
+        /// <param name="id">身份证</param>
+        /// <param name="name">名字</param>
+        /// <param name="birthPlace">出生地</param>
+        /// <param name="familyAddress">家庭住址</param>
+        /// <param name="age">年龄</param>
+        /// <param name="gender">性别</param>
+        public void Input(string id, string name, string birthPlace,
+            string familyAddress,int age,Gender gender)
+        {
+            ID = id;
+            Name = name;
+            BirthPlace = birthPlace;
+            FamilyAddress = familyAddress;
+            Age = age;
+            Gender = gender;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("姓名：{0}, 身份证：{1}", Name, ID);
+            Console.WriteLine("出生地：{0}, 家庭住址：{1}", BirthPlace, FamilyAddress);
+            Console.WriteLine("年龄：{0}, 性别：{1}", Age, Gender);
+        }
+    }
+
+}
